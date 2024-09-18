a53e53c488d4b647737db860bf5934a8af61d3b3
gabrielmulleer <emaildegabrielm@gmail.com>
2024-09-18 19:13:09 -0300

    feat: create button component

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@/components/ui/Button/button'
 import './styles.scss'
 
 export default function Todos() {
   return (
     <div>
       <h1>My Todo List</h1>
+      <Button variant="primary">Add Todo</Button>
+      <Button>Cancel</Button>
+      <Button variant="delete">Delete</Button>
     </div>
   )
 }
---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import './Button.scss' // Supondo que o estilo SCSS esteja aqui
+
+// Definindo as variantes como tipos
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'delete' | 'default'
+  size?: 'sm' | 'lg' | 'default'
+  rounded?: 'sm' | 'lg' | 'xl' | 'full' | 'default'
+}
+
+// Componente Button com forwardRef
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      variant = 'default',
+      size = 'default',
+      rounded = 'default',
+      className,
+      ...props
+    },
+    ref,
+  ) => {
+    const variantClass = `btn-${variant}`
+    const sizeClass = `btn-size-${size}`
+    const roundedClass = `btn-rounded-${rounded}`
+
+    return (
+      <button
+        className={`btn ${variantClass} ${sizeClass} ${roundedClass} ${className}`}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
+
+Button.displayName = 'Button'
+
+export { Button }
