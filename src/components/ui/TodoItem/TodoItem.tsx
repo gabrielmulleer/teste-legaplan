e676c6a326f9f5e8364302928bccf09bde1b4839
gabrielmulleer <emaildegabrielm@gmail.com>
2024-09-19 17:46:37 -0300

    refactor: change TodoItem component to use DeleteTodo component for delete functionality

--- src/components/ui/TodoItem/TodoItem.tsx
+++ src/components/ui/TodoItem/TodoItem.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import './TodoItem.scss'
+import DeleteTodo from '@/app/todos/_components/delete-todo'
 
 interface TodoItemProps {
   children: React.ReactNode
@@ -25,23 +26,24 @@ const TodoItem: React.FC<TodoItemProps> = ({
         className={`checkbox`}
       />
       <span className={`text`}>{children}</span>
-
-      <button onClick={onDelete} className={`deleteButton`}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <polyline points="3 6 5 6 21 6"></polyline>
-          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-        </svg>
-      </button>
+      <DeleteTodo onDelete={onDelete}>
+        <button className={`deleteButton`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <polyline points="3 6 5 6 21 6"></polyline>
+            <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+          </svg>
+        </button>
+      </DeleteTodo>
     </div>
   )
 }
